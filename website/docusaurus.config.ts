6b0fcfcf21c2f4e254114c9447e61f1498ec2a90
Charles Berube <cberube@gmail.com>
2025-06-09 09:49:38 -0700

    Someday, there are going to be docs

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://spinfoam-games.github.io/inexorable/',
+  url: 'https://spinfoam-games.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/inexorable/',
